99bb55daf59c1c8e7b83ad0a67aa87ccdf39c94a
Pawel Wozny <pawel.wozny@protonmail.com>
2023-08-27 11:37:35 +0200

    init

---
+++ src/language/daedalus-validator.ts
@@ -0,0 +1,31 @@
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import type { DaedalusAstType, Person } from './generated/ast.js';
+import type { DaedalusServices } from './daedalus-module.js';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: DaedalusServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.DaedalusValidator;
+    const checks: ValidationChecks<DaedalusAstType> = {
+        Person: validator.checkPersonStartsWithCapital
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class DaedalusValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
